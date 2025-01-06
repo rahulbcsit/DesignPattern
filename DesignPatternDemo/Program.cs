03aa3930f512a6cdc360a5c929a260f107fd9b5d
Rahul Gupta-Gunners <rahulbcsit@hotmail.com>
2025-01-05 21:21:50 -0600

    added FAcotryMethoDemo

--- DesignPatternDemo/Program.cs
+++ DesignPatternDemo/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Runtime.InteropServices;
+using FactoryMethod;
+
+ PlayerCreator playerFactoryCreator;
+
+ if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+ {
+    playerFactoryCreator = new WindowsPlayerCreator();
+ }
+else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+{
+    playerFactoryCreator = new LinuxPlayerCreator();
+}
+else
+{
+    throw new Exception("only mac and winodw ssupport ");
+
+
+}
+
+System.Console.WriteLine("please give the file path");
+var filePath = Console.ReadLine() ?? "defult.mg";
+await playerFactoryCreator.CreatePlayer().Play(filePath);
+Console.ReadKey();
